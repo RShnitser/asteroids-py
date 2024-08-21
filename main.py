d5e3983c3d8419c912d3fbe64043f9500c5c297b
RShnitser <romanshnitser@hotmail.com>
2024-08-21 17:29:54 -0400

    add deltatime and circle

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,6 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
