94ab2496e69caccd8631af254827950e80d32da5
RShnitser <romanshnitser@hotmail.com>
2024-08-24 10:14:34 -0400

    add splitting

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
             for s in shots:
                 if a.check_collision(s):
                     s.kill()
-                    a.kill()
+                    a.split()
         for d in drawable:
             d.draw(screen)
         pygame.display.flip()
